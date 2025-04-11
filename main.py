b5ad934e24a1d961179b09e9553d51a7837b70e9
julee sahu <juleesahu@gmail.com>
2025-04-12 00:00:51 +0530

    new changed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/inventory")
 def check_inventory():
-    return {"message": "Medical inventory data here!"}
+    return {"message": "Medical julee data here!"}
 
 # Required to run Uvicorn when the script is executed directly
 if __name__ == "__main__":
